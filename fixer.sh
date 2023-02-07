b707685cbb47e21c9ed534ae2c0ad535ad58ed71
Daniel Vega <lyradeperseo@gmail.com>
2023-02-07 02:57:17 -0500

    Create fixer.sh
    Run these commands on your WSL terminal and your network issues will be resolved.

---
+++ fixer.sh
@@ -0,0 +1,4 @@
+sudo rm /etc/resolv.conf
+sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
+sudo bash -c 'echo "[network]" > /etc/wsl.conf'
+sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
